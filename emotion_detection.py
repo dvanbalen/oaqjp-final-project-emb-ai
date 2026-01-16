266d338ae0a773ac71e8b9dc4cd88a30adef1b1c
dvanbalen <dvanbalen@skillsnetwork>
2026-01-16 17:37:13 -0500

    added output formatting

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyse):
@@ -9,5 +10,18 @@ def emotion_detector(text_to_analyse):
           'watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = { "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock" }
     input_json = { "raw_document": { "text": text_to_analyse } }
+    # Call AI service
     response = requests.post(url, headers = header, json = input_json)
-    return response.text
+    # Convert response text to dictionary
+    resp_dict = json.loads(response.text)
+    # Extract emotions from response
+    emotions = resp_dict['emotionPredictions'][0]['emotion']
+    # Find emotion given the highest certainty score by the AI service
+    dominant_emotion = ""
+    dominant_score = 0
+    for emotion, score in emotions.items():
+        if score > dominant_score:
+            dominant_score = score
+            dominant_emotion = emotion
+    emotions['dominant_emotion'] = dominant_emotion
+    return emotions
